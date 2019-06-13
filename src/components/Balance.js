1b05adacec486313893508f043221fbb0fcdc764
youssef <youssef.sadek910@gmail.com>
2019-06-13 17:17:58 +0100

    saved to transfer

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,11 +5,21 @@ class Balance extends Component {
   constructor() {
     super();
     this.state = {
-      alternateCurrency: "USD"
+      alternateCurrency: "USD",
+      selectedCurrency: null
     };
   }
 
+  handelChange = (event) => {
+    this.setState({ selectedCurrency: event.target.value });
+  };
+  // componentDidMount(){fetch(" https://api.exchangeratesapi.io/latest HTTP/1.1").then(res=>res.json()).then(data=>console.log(data))
+
+  // }
+  // CalculateBalanceonselected =()=>{this.props.total*1.3}}
+
   render() {
+    console.log(this.state.selectedCurrency);
     return (
       <div className="Balance">
         <h2 className="Balance-title">
@@ -17,8 +27,8 @@ class Balance extends Component {
           <span className="Balance-total">£{this.props.total}</span>
         </h2>
         <div className="Balance-alt">
-          Your balance is ??? in
-          <select defaultValue={this.state.alternateCurrency}>
+          Your balance is {this.props.total * 1.3} in{this.state.selectedCurrency}
+          <select onChange={this.handelChange} defaultValue={this.state.alternateCurrency}>
             {this.props.currencies.map((currency, index) => (
               <option key={index}>{currency}</option>
             ))}
