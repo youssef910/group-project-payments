73e16dadd1eba18cb8a7d1032a819a679d25d39a
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-06-20 17:14:58 +0100

    completed Task 1

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,12 +2,36 @@ import React, { Component } from "react";
 import "./Balance.css";
 
 class Balance extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      alternateCurrency: "USD"
+      alternateCurrency: "USD",
+      balanceInAlternateCurrency: props.total
     };
   }
+
+  componentDidMount() {
+    fetch(`https://api.exchangeratesapi.io/latest?base=GBP`)
+      .then((res) => res.json())
+      .then((data) => {
+        const alternateCurrency = this.state.alternateCurrency;
+        const exchangeRates = data.rates[alternateCurrency];
+        const newBalance = this.props.total;
+        this.setState({
+          balanceInAlternateCurrency: (exchangeRates * newBalance).toFixed(2)
+        });
+      });
+    console.log("alternateCurrency");
+    console.log("currancies");
+  }
+
+  handleCurrencyUpdate = (event) => {
+    const handleChange = event.target.value;
+    this.setState({
+      alternateCurrency: handleChange
+    });
+    this.componentDidMount();
+  };
 
   render() {
     return (
@@ -16,9 +40,11 @@ class Balance extends Component {
           Your account balance is
           <span className="Balance-total">£{this.props.total}</span>
         </h2>
+
         <div className="Balance-alt">
-          Your balance is ??? in
-          <select defaultValue={this.state.alternateCurrency}>
+          Your balance is {this.state.balanceInAlternateCurrency} shows the balance on the webpage
+          in
+          <select defaultValue={this.state.alternateCurrency} onChange={this.handleCurrencyUpdate}>
             {this.props.currencies.map((currency, index) => (
               <option key={index}>{currency}</option>
             ))}
